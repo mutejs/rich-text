c54f7b62597ae08e31aa6bdba7bd9865e77051b4
Todd Kennedy <todd@selfassembled.org>
2018-11-05 08:08:03 -0800

    rust is odd

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,48 @@
+#[derive(Default,Clone)]
 struct Op {
-  delete: usize,
-  insert: String,
-  retain: usize
+  delete: Option<usize>,
+  insert: Option<String>,
+  retain: Option<usize>
 }
 
 impl Op {
+  fn delete (len:usize) -> Self {
+    Op {
+      delete: Some(len),
+      ..Default::default()
+    }
+  }
+
+  fn retain (len:usize) -> Self {
+    Op {
+      retain: Some(len),
+      ..Default::default()
+    }
+  }
+
+  fn insert (text:String) -> Self {
+    Op {
+      insert: Some(text.to_string()),
+      ..Default::default()
+    }
+  }
+
   fn iterator (op:Vec<Op>) -> Iterator {
     Iterator::new(op)
+
   }
-  fn len (op:Op) -> usize {
-    return match op {
-      delete => op.delete,
-      insert => op.insert.len(),
-      retain => op.retain
-    }
+
+  fn len (op:Op) -> Option<usize> {
+    let default:Option<usize> = Some(0);
+    if Some(op.delete) != None {
+      return op.delete
+    } else if Some(op.insert) != None {
+      return op.insert.as_ref().map(|s| s.len())
+    } else if Some(op.retain) != None {
+      return op.retain
+    } else {
+      return default
+    };
   }
 }
 
@@ -35,8 +64,9 @@ impl Iterator {
   fn peekLength(&self) -> usize {
     // if our index is in bounds
     if self.ops.len() < self.index && self.index > 0 {
-      let op = self.ops[self.index];
-      return Op::len(op) - self.offset;
+      let op = self.ops[self.index].clone();
+      let opLen = Op::len(op).expect("Op had no value");
+      return opLen - self.offset;
     } else {
       return 0
     }
